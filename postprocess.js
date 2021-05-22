5bfc7a9b8cd5ef28c6bc15020bf81e1a51ff73d1
Andrew Skinner <ajskinner@gmail.com>
2021-05-22 22:09:36 +1000

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -9,7 +9,7 @@ const json = await readJSON(filename)
 // Step 2: Filter specific data we want to keep and write to a new JSON file
 const solarInfo = Object.values(json); // convert property values into an array
 solarInfo.shift()
-filteredSolarInfo = solarInfo
+const filteredSolarInfo = solarInfo
 
 // Step 3. Write a new JSON file with our filtered data
 const newFilename = `processed-solar.json` // name of a new file to be saved
